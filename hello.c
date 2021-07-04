508133a3dc3dc24bb8fd0d7ab7cdef9c71e66ccd
zhaowentaoa <976053198@qq.com>
2021-07-04 18:42:39 +0800

    修改dev分支节点代码

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
 {
-
+    printf("\n");
+     printf("dev code\n");
     return 0;
 }
--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 int main()
 {
 
-    printf("hello\n");
+    printf("\n");
 
     return 0;
 }   
